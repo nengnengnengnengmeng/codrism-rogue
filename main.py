04a35b59e4fe8aa788978c5dfa85f261525f29d6
CODRISM <yj.codrism@gmail.com>
2025-11-30 19:38:36 +0900

    조작법 변경

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ map_data = generate_map()
 
 #start_screen() # 나중에 활성화
 #player_name = get_player_name()
-player = Player(1, 1, "{player_name}")
+player = Player(3, 3, "{player_name}")
 message = "Hello {player.name}"
 
 # 입력 스레드 시작
