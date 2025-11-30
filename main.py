638016b0be393d0f52edea4a442d460a2d7d3d6d
1011010100000 <romimcgg@gmail.com>
2025-11-30 17:55:01 +0900

    방 생성

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from utils import generate_map, move_player
+from utils import generate_map
 from screens import start_screen, get_player_name
 from const import *
 import input_handler
@@ -9,16 +9,12 @@ from player import Player
 TPS = 5
 TICK_TIME = 1 / TPS
 
-map_data = generate_map(MAP_HEIGHT, MAP_WIDTH)
-player = Player(1, 1, "{player_name}")
-message = "Hello {player.name}"
-
-dx = 0
-dy = 0
+map_data = generate_map()
 
 #start_screen() # 나중에 활성화
 #player_name = get_player_name()
-
+player = Player(1, 1, "{player_name}")
+message = "Hello {player.name}"
 
 # 입력 스레드 시작
 os.system('cls')
@@ -40,14 +36,11 @@ while True:
     # 입력 처리
     dx = 0
     dy = 0
-
     for key in input_handler.pressed_keys:
         if key in MOVES:
             mx, my = MOVES[key]
             dx += mx
             dy += my
-            decision = key
-
     player.move(dx, dy, map_data)
 
     # 화면 출력
--- utils.py
+++ utils.py
@@ -1,15 +1,34 @@
 from const import *
+import random as rand
+class Room():
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+
 # generate_map
-def generate_map(height, width):
-    map = [['.' for _ in range(width)] for _ in range(height)]
-    for i in range(height):
-        map[i][0] = WALL
-        map[i][width-1] = WALL
+def generate_map():
+    map = [[WALL for _ in range(MAP_WIDTH)] for _ in range(MAP_HEIGHT)]
+    
+    rooms = []
+    cell_w = MAP_WIDTH // 3 # 26
+    cell_h = MAP_HEIGHT // 3 # 7
 
-    for i in range(width):
-        map[0][i] = WALL
-        map[height-1][i] = WALL
+    for row in range(3):
+        for col in range(3):
+            cell_x = col * cell_w # 0, 26, 52
+            cell_y = row * cell_h # 0, 7, 14
+            room_w = rand.randint(2, cell_w - 2) # 2-24
+            room_h = rand.randint(2, cell_h - 2) # 2-5
+            room_x = cell_x + rand.randint(1, cell_w - room_w - 1) # 1-23
+            room_y = cell_y + rand.randint(1, cell_h - room_h - 1) # 1-4
+            room = Room(room_x, room_y, room_x + room_w, room_y + room_h)
+            rooms.append(room)
 
-    map[1][1] = PLAYER
+    for room in rooms:
+        for y in range(room.y1, room.y2):
+            for x in range(room.x1, room.x2):
+                map[y][x] = FLOOR
 
     return map
